0db06690b9ddc9f4bf4eac07a0acea6f4540f2fd
Rainard Human <rainard.human@gmail.com>
2021-04-19 15:33:10 +0200

    added documentation

--- src/main/java/com/rainard/grindhouse/GrindhouseApplication.java
+++ src/main/java/com/rainard/grindhouse/GrindhouseApplication.java
@@ -1,8 +1,13 @@
 package com.rainard.grindhouse;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
@@ -11,5 +16,20 @@ public class GrindhouseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GrindhouseApplication.class, args);
 	}
+
+
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.version("1.0")
+						.title("GrindHouse")
+						.description("Backend API for Coffee Shop Demo")
+						.termsOfService("http://swagger.io/terms/")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
+
+
 
 }
